1f11af3b973a33d83fdfd0dcc0b0c6851458aa95
Sauravi Shrestha <sauravishrestha11gmail.com>
2026-04-24 11:48:17 +0545

    Changed the db name

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'gis_crystal');
+define('DB_NAME', 'inventory_db');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
