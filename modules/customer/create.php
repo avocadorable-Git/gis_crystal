8b56c4c38ca5003f3334eec3ad3339f6624ed134
Sauravi Shrestha <sauravishrestha11gmail.com>
2026-04-26 13:09:20 +0545

    Made specific folders for modules

--- modules/customer/create.php
+++
@@ -1,7 +0,0 @@
-<?php include '../../includes/header.php'; ?>
-<form method="POST" action="insert.php">
-Name: <input type="text" name="name" required>
-Price: <input type="number" name="price" required>
-<button type="submit">Save</button>
-</form>
-<?php include '../../includes/footer.php'; ?>
